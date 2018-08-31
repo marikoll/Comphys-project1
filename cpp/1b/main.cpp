b4bc7ac40d967c3c299b49c02011167de63d4b10
Gunnar <gunnartl@uio.no>
2018-08-31 12:12:51 +0200

    akkuratt bynt

---
+++ cpp/1b/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int n = 10;
+    double h = 1./(n+1);
+
+    double *a,*b,*c;
+
+
+    //cout << "Hello World!" << endl;
+    return 0;
+}
