0e5762613eb15890f30cd68187396d633c308c3d
Gunnar <gunnartl@uio.no>
2018-08-31 11:45:44 +0200

    Final Product

--- python/pythontest2.py
+++ python/pythontest2.py
@@ -1,6 +1,7 @@
 import numpy as np
+import time
 
-n = 10
+n = 100000
 
 x = np.linspace(0,1,n+2)
 h = 1./(n+1)
@@ -20,7 +21,9 @@ u = 1- (1-np.exp(-10))*x - np.exp(-10*x)
 d = h**2*f
 d[0]  = 0
 d[-1] = 0
-v = np.zeros(n)
+
+
+start = time.time()
 
 for i in range(1,n):
 	cof   = a[i]/b[i-1]
@@ -28,15 +31,18 @@ for i in range(1,n):
 	b[i] -= cof*c[i-1]
 	d[i+1] -= cof*d[i]
 
+d[-2] = d[-2]/b[-1]
+
 for i in range(1,n):
-	print(-i-1,-i)
-	cof      = c[-i-1]/b[-i]
-	c[-i-1] -= cof*b[-i]
-	d[-i-2] -= cof*d[-i-1]
+	#cof      = c[-i-1]/b[-i]
+	#c[-i-1] -= cof*b[-i]
+	d[-i-2] -= (c[-i-1]/b[-i])*d[-i-1]
 
 d[1:-1] = d[1:-1]/b
+end = time.time()
 
-print(d)
-import matplotlib.pyplot as plt
-plt.plot(x,u,x,d)
-plt.show()
+print(end-start)
+
+#import matplotlib.pyplot as plt
+#plt.plot(x,u,x,d)
+#plt.show()
